3683fa25be91c5a2be781aaf3c6997e8ccf386bf
Kijakal <89953107+Kijakal@users.noreply.github.com>
2023-02-18 16:02:56 -0600

    deployedversion

---
+++ catalog/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from .models import BookInstance, Book, Author, Genre
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(LoginRequiredMixin, generic.ListView):
+    model = Book
+
+
+class BookDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Book
+
+
+class AuthorListView(LoginRequiredMixin, generic.ListView):
+    model = Author
+
+class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Author
